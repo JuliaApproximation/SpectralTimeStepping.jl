24d391e2a0df9fde2c0a73106e6faf71f9e03eed
Mikael Slevinsky <Richard.Slevinsky@umanitoba.ca>
2019-11-28 14:53:52 -0600

    update plot_spectrum.jl
    move plot_Allen_Cahn_Makie to plot_Allen_Cahn

--- NonlocalSphere/NonlocalSphere.md
+++ NonlocalSphere/NonlocalSphere.md
@@ -2,4 +2,4 @@
 
 This file contains the source code to recreate almost all of the data in the paper
 
-R. M. Slevinsky, H. Montanelli, and Q. Du, A spectral method for nonlocal diffusion operators on the sphere, 2018.
+R. M. Slevinsky, H. Montanelli, and Q. Du, [A spectral method for nonlocal diffusion operators on the sphere](https://doi.org/10.1016/j.jcp.2018.06.024), *J. Comp. Phys.*, **372**: 893--911, 2018.
--- NonlocalSphere/plot_spectrum.jl
+++ NonlocalSphere/plot_spectrum.jl
@@ -3,18 +3,18 @@ include("evaluate_lambda.jl")
 using PyPlot
 
 α = [-0.5;0.5]
-L = round.([Int], logspace(0,4,50))
+L = round.(Int, exp10.(range(0, stop=4, length=50)))
 λlap = [-ℓ*(ℓ+1) for ℓ in L]
 
 for i = 1:length(α)
     δ1 = 0.01
-    λ1 = [evaluate_lambda(ℓ, δ1, α[i]) for ℓ in L]
+    λ1 = evaluate_lambda(L[end]+1, α[i], δ1)[L.+1]
     δ2 = 0.1
-    λ2 = [evaluate_lambda(ℓ, δ2, α[i]) for ℓ in L]
+    λ2 = evaluate_lambda(L[end]+1, α[i], δ2)[L.+1]
     δ3 = 1.0
-    λ3 = [evaluate_lambda(ℓ, δ3, α[i]) for ℓ in L]
+    λ3 = evaluate_lambda(L[end]+1, α[i], δ3)[L.+1]
     δ4 = 2.0
-    λ4 = [evaluate_lambda(ℓ, δ4, α[i]) for ℓ in L]
+    λ4 = evaluate_lambda(L[end]+1, α[i], δ4)[L.+1]
 
     clf()
     loglog(L, -λlap, "-k", L, -λ1, ".k", L, -λ2, "xk", L, -λ3, "+k", L, -λ4, "--k")
